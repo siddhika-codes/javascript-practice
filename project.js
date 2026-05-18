8dcfa5d9e5fde3e9c49b249756ab5d1d93521eee
siddhika-codes <siddhikabishnoi76@gmail.com>
2026-05-18 20:38:49 +0530

    Added JavaScript mini projects

--- project.js
+++ project.js
@@ -39,3 +39,69 @@ let discountamount = productprice*quantity/10
 console.log(discountamount)
 let finalbill = total-discountamount
 console.log(finalbill)
+
+// Project 3 — Food Delivery Eligibility
+// Check:
+// restaurant open
+// distance less than 10 km
+// user logged in
+// Then decide:
+// "Order Accepted"
+let restaurantOpen = true;
+let distance = 10;
+let userLoggedin = true;
+if(restaurantOpen && distance <= 10 && userLoggedin) {
+    console.log("Order Accepted")
+} else {
+    console.log("Order Denied")
+}
+
+// Project 4 — Salary Bonus Calculator
+// Input:
+// employee salary
+// years of experience
+// Conditions:
+// high salary + high experience → big bonus
+// otherwise smaller bonus
+let employeeSalary = 95000;
+let experienceYear = 3;
+if(employeeSalary >= 85000 && experienceYear >= 2) {
+    console.log("big bonus")
+} else {
+    console.log("small bonus")
+}
+
+// Project 5 — Student Result Portal
+// Store:
+// marks of 5 subjects
+// Calculate:
+// total
+// percentage
+// grade
+// pass/fail
+let Hindi = 89;
+let English = 84;
+let Science = 78;
+let Socialscience = 79;
+let Computer = 93;
+
+let totalmarks = Hindi + English + Science + Socialscience + Computer
+console.log(totalmarks)
+let percentage = totalmarks/500*100
+console.log(percentage)
+if(percentage >= 33) {
+    console.log("PASS")
+} else {
+    console.log("FAIL")
+}
+if(percentage >= 90) {
+    console.log("A")
+} else if(percentage >= 80) {
+    console.log("B")
+} else if(percentage >= 70) {
+    console.log("C")
+} else if(percentage >= 60) {
+    console.log("D")
+} else {
+    console.log("FAIL")
+}
